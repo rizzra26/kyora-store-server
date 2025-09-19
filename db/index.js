2cce9341bc0e9a708ac887e936df5a2d9485bce6
rizzra26 <rizukirimu@gmail.com>
2025-09-19 22:06:55 +0700

    setting up before deploy

--- db/index.js
+++ db/index.js
@@ -1,13 +1,8 @@
-const mongoose = require('mongoose')
-const { urlDb } = require('../config')
+const mongoose = require("mongoose");
+const { urlDb } = require("../config");
 
-mongoose.connect(urlDb, {
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useCreateIndex: true,
-  useNewUrlParser: true 
-})
+mongoose.connect(urlDb);
 
-const db =  mongoose.connection
+const db = mongoose.connection;
 
-module.exports = db
+module.exports = db;
